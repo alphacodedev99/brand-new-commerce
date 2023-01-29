b4890ea6bcf7bc3a7757bfaa210f29c556f77924
AlphaDev <30984729+AlphaForza@users.noreply.github.com>
2023-01-29 13:57:05 +0100

    factory: fetch/category

--- src/components/CategoryHolder.jsx
+++ src/components/CategoryHolder.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fetchCategory } from '../utils/fetchCategory';
+
+// icons
+import MenuIcon from '@mui/icons-material/Menu';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllCategory } from '../store/sliceCategory';
 
 function CategoryHolder() {
-	return <div>CategoryHolder</div>;
+	// send data to redux-toolkit
+	const dispatch = useDispatch();
+	// get item from redux
+	const { category } = useSelector((state) => state.getCategory);
+
+	useEffect(() => {
+		fetchCategory().then((data) => dispatch(getAllCategory(data)));
+	}, []);
+
+	// for category selection
+	let firstFiveCategories = category.slice(0, 5);
+
+	return (
+		<div>
+			<div className='flex items-center gap-2'>
+				<MenuIcon className='mr-1 cursor-pointer' fontSize='large' />
+				<p className='mr-5'>All category</p>
+				<div className='flex gap-[26px]'>
+					{firstFiveCategories.map((item, index) => {
+						return (
+							<p key={index} className=''>
+								{item}
+							</p>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default CategoryHolder;
