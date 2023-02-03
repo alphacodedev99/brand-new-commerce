911a744e19da9a1a442b165b003a38e0addd17c3
Alkibijad <aleksandar04081@gmail.com>
2023-02-03 12:20:07 -0500

    new commit for responsive

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React from "react";
+import { BsEnvelope } from "react-icons/bs";
 
 function Subscribe() {
-	return <div className='subscribe'>
-		<h2>Subscribe on our newsletter</h2>
-		<p>Get daily news on upcoming offers form many suppliers all over the world</p>
-		<div className="form-container">
-			<input type="text" placeholder='Email' />
-			<button>Subscribe</button>
-		</div>
-	</div>;
+  return (
+    <div className="subscribe">
+      <h2>Subscribe on our newsletter</h2>
+      <p>
+        Get daily news on upcoming offers form many suppliers all over the world
+      </p>
+      <div className="form-container">
+        <div className="input-wrapper">
+          <BsEnvelope />
+          <input type="text" placeholder="E-Mail" />
+        </div>
+    
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
 }
 
 export default Subscribe;
