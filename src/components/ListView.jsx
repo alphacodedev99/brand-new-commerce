128b3a29f39ab0182baafbb85fa0f5fc730dca55
AlphaDev <30984729+AlphaForza@users.noreply.github.com>
2023-02-05 13:12:44 +0100

    Pagination and stars for reating of product.

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom'
+import ReactPaginate from 'react-paginate';
+import {Rating} from "@mui/material";
+import StarIcon from '@mui/icons-material/Star';
 function ListView({ activeView, allProduct }) {
-	console.log(allProduct);
+	// setup for pagination
+	const [currentItems, setCurrentItems] = useState([]);
+	const [pageCount, setPageCount] = useState(0);
+	const [itemOffset, setItemOffset] = useState(0);
+	const itemsPerPage = 6;
+
+	useEffect(() => {
+		const endOffset = itemOffset + itemsPerPage;
+		setCurrentItems(allProduct.slice(itemOffset, endOffset));
+		setPageCount(Math.ceil(allProduct.length / itemsPerPage))
+	}, [itemOffset,itemsPerPage,allProduct]);
+
+	const handlePageClick = (event) => {
+		const newOffset = (event.selected * itemsPerPage) % allProduct.length;
+		setItemOffset(newOffset);
+	};
 	return (
+		<>
+
 		<div
 			className={`${
 				activeView === 'listView'
 					? 'flex flex-col gap-5 mt-[20px]'
 					: 'grid grid-cols-3 gap-5 mt-[20px]'
 			}`}>
-			{allProduct.map((item,index)=>{
+			{currentItems.map((item,index)=>{
 				return(
 					<div key={index} className={`${activeView === 'listView' ? 'flex items-center gap-10 bg-white h-[230px] ' : 'bg-white flex flex-col items-center  p-[20px]  max-h-[400px] overflow-hidden'}`}>
 						<img src={item.thumbnail} alt={item.title} className={`${activeView === 'listView' ? 'h-full max-w-[200px] object-cover' : 'w-[230px] h-[230px] object-cover'}`} />
@@ -18,7 +38,23 @@ function ListView({ activeView, allProduct }) {
 							' w-full mt-[10px]'}`}>
 							{activeView === 'listView' ? <h3 className="text-[#1C1C1C] text-xl">{item.title}</h3> : null}
 							<p className="text-[20px] font-bold">${item.price}</p>
-							<span className="text-[#FF9017]">{item.rating}</span>
+							<div className="flex items-center gap-2">
+								<Rating
+									name="text-feedback"
+									value={item.rating}
+									readOnly
+									precision={0.5}
+									emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+								/>
+								<p className="text-[#ff9800]">{item.rating}</p>
+								{activeView === 'listView' ? (
+									<>
+										<p className="text-[#8B96A5] font-[400]"> - {item.stock} orders</p>
+										<p className="text-[#00B517]">Free Shipping</p>
+									</>
+								) : null}
+							</div>
+
 							<p className="text-[#505050] text-lg">{activeView === 'listView' ? item.description : item.category }</p>
 							{activeView === 'listView' ? <Link to='/' className="text-[#0D6EFD]">View details</Link> : null}
 						</div>
@@ -26,6 +62,23 @@ function ListView({ activeView, allProduct }) {
 				)
 			})}
 		</div>
+
+		{/*	PAGINATION*/}
+			<ReactPaginate
+				className="flex gap-2 items-center justify-center mt-5"
+				pageClassName="bg-[#fff] border border-[#ddd] p-3 flex items-center justify-center"
+				activeClassName="bg-[#DEE2E7]"
+				previousClassName="bg-[#fff] border border-[#ddd] p-3"
+				nextClassName="bg-[#fff] border border-[#ddd] p-3"
+				breakLabel="..."
+				nextLabel="next >"
+				onPageChange={handlePageClick}
+				pageRangeDisplayed={5}
+				pageCount={pageCount}
+				previousLabel="< previous"
+				renderOnZeroPageCount={null}
+			/>
+		</>
 	);
 }
 
